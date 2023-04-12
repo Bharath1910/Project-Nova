03bae4301d814651f632d2363d75c09fe6df1073
Bharath1910 <av.bharath@proton.me>
2023-04-12 23:03:34 +0530

    added run command for the first option

--- wss.sh
+++ wss.sh
@@ -4,8 +4,8 @@ options="Option 1\nOption 2\nOption 3"
 selected=$(echo -e "$options" | rofi -dmenu)
 
 case $selected in
-    "Option 1")
-        # Replace this with the command you want to run for option 1
+    "Regular")
+        python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work regular
         ;;
     "Option 2")
         # Replace this with the command you want to run for option 2
