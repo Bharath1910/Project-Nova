54b507a5523d1c5aa9cb9d5c6d20ff51071ed935
Bharath1910 <av.bharath@proton.me>
2023-04-13 17:51:03 +0530

    thrown the i3 commands into a function with loops

--- wsExecute.py
+++ wsExecute.py
@@ -8,18 +8,29 @@ args = parser.parse_args()
 
 i3 = i3ipc.Connection()
 
+def execute(payload):
+    for app, delay in payload:
+        i3.command(app)
+        time.sleep(delay)
+
 if args.work == "regular":
-    i3.command("exec firefox-developer-edition --new-tab https://www.youtube.com")
-    time.sleep(1)
+    execute([
+        ["exec firefox-developer-edition --new-tab https://www.youtube.com", 5],
+        ["exec spotify", 4],
+        ["exec discord", 2],
+        ["exec telegram-desktop", 0]
+    ])
+
+    # i3.command("exec firefox-developer-edition --new-tab https://www.youtube.com")
+    # time.sleep(2)
     
-    i3.command("exec spotify")
-    time.sleep(4)
+    # i3.command("exec spotify")
+    # time.sleep(4)
 
-    i3.command("exec discord")
-    time.sleep(2)
+    # i3.command("exec discord")
+    # time.sleep(2)
 
-    i3.command("exec telegram-desktop")
-
+    # i3.command("exec telegram-desktop")
 
 else:
     print("nope")
