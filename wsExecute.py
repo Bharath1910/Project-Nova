306f58c129f2bd0296f31f6671c6d193d9d03b70
Bharath1910 <av.bharath@proton.me>
2023-04-13 16:47:17 +0530

    changed priorities and delayed execution of resource heavy apps

--- wsExecute.py
+++ wsExecute.py
@@ -9,23 +9,17 @@ args = parser.parse_args()
 i3 = i3ipc.Connection()
 
 if args.work == "regular":
-    i3.command("workspace 2")
     i3.command("exec firefox-developer-edition --new-tab https://www.youtube.com")
-    time.sleep(0.1)
-
-    i3.command("workspace 3")
-    i3.command("exec discord")
-    time.sleep(0.1)
-
-    i3.command("workspace 4")
-    i3.command("exec telegram-desktop")
-    time.sleep(0.1)
-
-    i3.command("workspace 5")
+    time.sleep(1)
+    
     i3.command("exec spotify")
-    time.sleep(0.1)
+    time.sleep(4)
 
-    i3.command("workspace 2")
+    i3.command("exec discord")
+    time.sleep(2)
+
+    i3.command("exec telegram-desktop")
+
 
 else:
     print("nope")
