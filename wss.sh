890df664f2fe13f4879482368baf94532598b0d4
Bharath1910 <av.bharath@proton.me>
2023-04-13 18:11:17 +0530

    added 2nd option

--- wss.sh
+++ wss.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-options="Regular\nOption 2\nOption 3"
+options="Regular\nDevelopment\nOption 3"
 selected=$(echo -e "$options" | rofi -dmenu)
 
 case $selected in
     "Regular")
         python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work regular
         ;;
-    "Option 2")
-        # Replace this with the command you want to run for option 2
+    "Development")
+        python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work development
         ;;
     "Option 3")
         # Replace this with the command you want to run for option 3
