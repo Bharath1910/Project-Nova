7f8a85d47f0b9d5ea7e71401ff7eb1ae1a6364a2
Bharath1910 <av.bharath@proton.me>
2023-04-21 22:33:27 +0530

    re wrote selector in python

---
+++ wss.py
@@ -0,0 +1,12 @@
+from pydymenu import rofi
+import os
+
+workspaces = ["development", "regular"]
+selectedWorkspace = rofi(workspaces, prompt="Select workspace")
+
+
+if selectedWorkspace[0] == "regular":
+    os.system("python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work regular")
+
+elif selectedWorkspace[0] == "development":
+    os.system("python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work development")
