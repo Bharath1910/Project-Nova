081c0ac04bec695b715b6e01fb9b2858f7de0b38
Bharath1910 <av.bharath@proton.me>
2023-04-22 21:58:59 +0530

    seperated base path into its own variable

--- wss.py
+++ wss.py
@@ -3,12 +3,13 @@
 from pydymenu import rofi
 import os
 
+basePath = "/home/bharath/Documents/Codes/projects/ProjectNova"
 workspaces = ["development", "regular"]
 selectedWorkspace = rofi(workspaces, prompt="Select workspace")
 
 
 if selectedWorkspace[0] == "regular":
-    os.system("python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work regular")
+    os.system(f"python3 {basePath}/wsExecute.py --work regular")
 
 elif selectedWorkspace[0] == "development":
-    os.system("python3 /home/bharath/Documents/Codes/projects/ProjectNova/wsExecute.py --work development")
+    os.system(f"python3 {basePath}wsExecute.py --work development")
