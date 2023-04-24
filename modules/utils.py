a2f6ac2c884729b6a79be98d319ac38734b328f5
Bharath1910 <av.bharath@proton.me>
2023-04-24 21:11:59 +0530

    completed the following method 1. parseConfiguration: now parses the global configuration file for nova 2. parseJSON: parses workspace.json file (Might want to rename the method) 3. composei3Command: parses the JSON and composes the i3 Command 4. invokei3Command: once called, it executes the given workspace commands in i3wm

--- modules/utils.py
+++ modules/utils.py
@@ -1,17 +1,41 @@
+import json
+import i3ipc
+
 class Utils:
-    def parseConfiguration(self):
-        pass
+    def __init__(self):
+        self.i3 = i3ipc.Connection()
 
+    def parseConfiguration(self, configFile="~/.config/nova/config.json"):
+        with open(configFile, "r") as file:
+            config = json.load(file)
+            self.projectDirectories = config["projectDirectories"]
+            
     def parseJSON(self):
-        pass
-
+        with open("workspaces.json", "r") as file:
+            workspaceConfig = json.load(file)
+            return workspaceConfig
+            
     def composei3Command(self):
-        pass
+        workspaceConfig = self.parseJSON()
+        composedConfig = {}
 
-    def invokei3Commands(self):
-        pass
+        for workspaceName, commands in workspaceConfig.items():
+            composedCommands = []
+            for commandWorkspaceDelay in commands:
+                i3Command = f"exec {commandWorkspaceDelay[0]}; workspace {commandWorkspaceDelay[1]}"
+                composedCommands.append(i3Command)
 
-    def isWindowExists(self, window):
-        pass
+                delay = commandWorkspaceDelay[2]
+                composedCommands.append(f"exec sleep {delay}")
 
+            composedConfig[workspaceName] = composedCommands
+        
+        return composedConfig
 
+    def invokei3Commands(self, workspace):
+        composedConfig = self.composei3Command()
+        if workspace not in composedConfig:
+            return
+            
+        for command in composedConfig[workspace]:
+            self.i3.command(command)
